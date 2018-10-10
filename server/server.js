d45df87649e81d4dccdb6e703d0df6b650f6c389
David Trotz <davidtrotz@olivetree.com>
2018-10-10 15:09:01 -0700

    Adding basic connect and disconnect

--- server/server.js
+++ server/server.js
@@ -1,15 +1,28 @@
 
 const express = require('express');
 const path = require('path');
-const publicPath = path.join(__dirname, '../public');
+const http = require('http');
+const socketIO = require('socket.io');
 
-const app = express();
+const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
 app.use(express.static(publicPath));
+
+io.on('connection', (socket) => {
+   console.log('New user connected.');
+    socket.on('disconnect', () => {
+        console.log('User disconnected.');
+    });
+});
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // APP
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Started on port ${port}`);
 })
