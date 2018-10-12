f4cd1d0847cd83c53c647c8adf7db1ba46ba981b
David Trotz <davidtrotz@olivetree.com>
2018-10-12 13:54:12 -0700

    Adding login page
    Lesson 121

---
+++ public/js/chat.js
@@ -0,0 +1,81 @@
+const  socket = io();
+
+function scrollToBottom () {
+    // Selectors
+    const messages = jQuery('#messages');
+    const newMessage = messages.children('li:last-child');
+    // Heights
+    const clientHeight = messages.prop('clientHeight');
+    const scrollTop = messages.prop('scrollTop');
+    const scrollHeight = messages.prop('scrollHeight');
+    const newMessageHeight = newMessage.innerHeight();
+    const lastMessageHeight = newMessage.prev().innerHeight();
+
+    if( clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+
+}
+
+socket.on('connect', function () {
+    console.log('Connected to server');
+});
+
+socket.on('disconnect', function () {
+    console.log('Disconnected from server');
+});
+
+socket.on('newMessage', function (message) {
+    const formattedTime = moment(message.createdAt).format('h:mm a');
+    const template = jQuery('#message-template').html();
+    const html = Mustache.render(template, {
+        from: message.from,
+        text: message.text,
+        createdAt: formattedTime
+    });
+    jQuery('#messages').append(html);
+    scrollToBottom();
+});
+
+socket.on('newLocationMessage', function (message) {
+    const formattedTime = moment(message.createdAt).format('h:mm a');
+    const template = jQuery('#location-message-template').html();
+    const html = Mustache.render(template, {
+        from: message.from,
+        url: message.url,
+        createdAt: formattedTime
+    });
+    jQuery('#messages').append(html);
+    scrollToBottom();
+});
+
+jQuery('#message-form').on('submit', function (evt) {
+    evt.preventDefault();
+    const messageTextBox = jQuery('[name=message]');
+    socket.emit('createMessage', {
+        from:'User',
+        text:messageTextBox.val()
+    }, function () {
+        messageTextBox.val('')
+    });
+});
+
+const locationButton = jQuery('#send-location');
+
+locationButton.on('click', function () {
+    if(!navigator.geolocation) {
+        return alert('Geolocation not supported on your browser.');
+    }
+
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
+    navigator.geolocation.getCurrentPosition((position) => {
+        locationButton.removeAttr('disabled').text('Send Location');
+        socket.emit('createLocationMessage', {
+            latitude:position.coords.latitude,
+            longitude:position.coords.longitude,
+        });
+    },(error) => {
+        locationButton.removeAttr('disabled').text('Send Location');
+        alert('Can\'t determine your location.');
+    })
+});
